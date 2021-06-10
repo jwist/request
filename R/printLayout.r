3250a2f4aceebfe3f127e4f9b4cc8ea46ec2ea23
jul <jul@anpc3>
2021-06-10 09:40:10 +0800

    fix issue #3 with display of missing tubes in layout for preparation

--- R/printLayout.r
+++ R/printLayout.r
@@ -4,16 +4,23 @@
 #' @export
 #' @importFrom htmlTable addHtmlTableStyle htmlTable
 #' @importFrom magrittr %>%
-printLayout <- function(selectedSamples, boxDim = c(8, 10)) {
+printLayout <- function(selectedSamples, boxDim = c(8, 10), by = "row", chk = FALSE ) {
   cat("<p style=\"page-break-before: always\">")
   plateList <- levels(factor(selectedSamples$plateID))
+  platePositions <- getPlatePos(boxDim = boxDim, by = by)
   for (plate in 1:length(plateList)) {
     ss <- selectedSamples[selectedSamples$plateID == plateList[plate],]
+    if (chk) {
+      ss <- ss[sample(nrow(ss), 3), ]
+    }
+    ep <- data.frame(wellPos = findEmptyPositions(ss$wellPos, boxDim = boxDim, by = by))
+    ss <- merge(ep, ss, by = "wellPos", all = TRUE) # adding missing slots
+    ss[is.na(ss)] <- " "
+
     position <- posToRC(ss$wellPos)
     F <- sort(as.numeric(position[,2]), index.return = TRUE)$ix
     ss <- ss[F,]
-    # F <- sort(position[,1], index.return = TRUE)$ix
-    # ss <- ss[F,]
+
     vec <- c(paste0( "", ss$tubeLabel, "",
                      "<br>", "<b>", ss$plateName, "</b> ",
                      ss$tubePos, "(" , ss$wellPos, ")<br>"),
